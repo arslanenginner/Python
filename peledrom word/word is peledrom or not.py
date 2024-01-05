0e7a4bc75b66df58974d5c2e860417e139ff9a83
Muhammad Arslan Zulfiqar <140038833+arslanenginner@users.noreply.github.com>
2024-01-05 22:55:09 +0500

    Delete peledrom word directory

--- peledrom word/word is peledrom or not.py
+++
@@ -1,9 +0,0 @@
-#Define a function.
-def peledrom (input_string):
-# read the word in reverse order
-   reserve_word=input_string[::-1]
-   return input_string==reserve_word
-# if word is correct in reverse order display true
-input=input("Enter a peledrom word:")
-output=peledrom(input)
-print(output)
